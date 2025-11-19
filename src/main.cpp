2e6438dc22f7f50209a82c124ce7454bb2bf92bb
abrka <ifradnashit@gmail.com>
2025-11-19 19:16:27 +0600

    use cmake to configure opengl version

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "GLApp/application.h"
 
+
 int main() {
-	GLApp::Application app{ 800,800,4,6 };
+	GLApp::Application app{ 800,800,OPENGL_VERSION_MAJOR,OPENGL_VERSION_MINOR };
 	app.run();
 }
