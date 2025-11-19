3eaa859193e952f3a19f5614f74efb36e63d5667
abrka <ifradnashit@gmail.com>
2025-11-19 19:36:55 +0600

    downgrade to c++20 and use tortan llama's implementation of std::expected

--- src/GLRenderer/shader_builder.h
+++ src/GLRenderer/shader_builder.h
@@ -2,7 +2,8 @@
 
 #include <memory>
 #include <filesystem>
-#include <expected>
+
+#include <tl/expected.hpp>
 
 #include "GL3D/shader_program.h"
 #include "utils.h"
@@ -13,14 +14,14 @@ namespace GLRenderer {
 			frag_shader_file_not_found,
 			vert_shader_file_not_found
 		};
-		std::expected<std::unique_ptr<GL3D::ShaderProgram>, ShaderBuilderError> build(std::filesystem::path frag_shader_filepath, std::filesystem::path vertex_shader_filepath) {
+		tl::expected<std::unique_ptr<GL3D::ShaderProgram>, ShaderBuilderError> build(std::filesystem::path frag_shader_filepath, std::filesystem::path vertex_shader_filepath) {
 			auto frag_shader_str = GLUtils::read_string_from_filepath(frag_shader_filepath);
 			if (!frag_shader_str.has_value()) {
-				return std::unexpected(ShaderBuilderError::frag_shader_file_not_found);
+				return tl::unexpected(ShaderBuilderError::frag_shader_file_not_found);
 			}
 			auto vert_shader_str = GLUtils::read_string_from_filepath(vertex_shader_filepath);
 			if (!vert_shader_str.has_value()) {
-				return std::unexpected(ShaderBuilderError::vert_shader_file_not_found);
+				return tl::unexpected(ShaderBuilderError::vert_shader_file_not_found);
 			}
 			auto vertex_shader = GL3D::Shader<GL3D::VertexShaderTag>{ vert_shader_str.value()};
 			auto fragment_shader = GL3D::Shader<GL3D::FragmentShaderTag>{ frag_shader_str.value() };
