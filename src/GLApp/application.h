2317968df65b7e0bd043a477d2db0b8aa2a8982b
abrka <ifradnashit@gmail.com>
2025-11-19 18:48:59 +0600

    add fps counter to window

--- src/GLApp/application.h
+++ src/GLApp/application.h
@@ -17,6 +17,9 @@ namespace GLApp {
 		std::unique_ptr<GLRenderer::Renderer> renderer{};
 
 	private:
+		const double fps_set_title_delay = 0.5;
+		double last_time_fps_was_set{};
+	private:
 		static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 			glViewport(0, 0, width, height);
 		}
@@ -30,8 +33,21 @@ namespace GLApp {
 		Application& operator=(const Application& rhs) = delete;
 		void run() {
 			while (!glfwWindowShouldClose(window->glfw_window)) {
+				double prev_time = glfwGetTime();
 				renderer->render();
 				glfw_frame_end(window->glfw_window);
+				double delta = glfwGetTime() - prev_time;
+				double fps = 1 / delta;
+
+				
+				
+				if (glfwGetTime() - last_time_fps_was_set > fps_set_title_delay) {
+					std::string title = "fps: " + std::to_string(fps);
+					glfwSetWindowTitle(window->glfw_window, title.c_str());
+					last_time_fps_was_set = glfwGetTime();
+				}
+
+				
 			}
 		}
 	};
