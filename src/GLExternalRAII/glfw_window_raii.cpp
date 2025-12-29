5f614c2512b0fadc33f71564b5b8ccaf8f1afc6d
abrka <ifradnashit@gmail.com>
2025-12-29 07:59:45 +0600

    use std::function as glfw callback in Window class

--- src/GLExternalRAII/glfw_window_raii.cpp
+++ src/GLExternalRAII/glfw_window_raii.cpp
@@ -1,9 +1,14 @@
 #include "glfw_window_raii.h"
+#include "glfw_window_raii.h"
+#include "glfw_window_raii.h"
 #include "GLExternalUtils/glfw_util.h"
 
 namespace GLExternalRAII {
 	Window::Window(int window_width, int window_height, int opengl_version_major, int opengl_version_minor) {
 		glfw_window = GLExternalUtils::glfw_init(window_width, window_height, opengl_version_major, opengl_version_minor);
+		glfwSetWindowUserPointer(glfw_window, (void*)this);
+		glfwSetKeyCallback(glfw_window, glfw_key_callback);
+		glfwSetFramebufferSizeCallback(glfw_window, glfw_framebuffer_size_callback);
 	}
 	bool Window::is_running() const {
 		return !glfwWindowShouldClose(glfw_window);
@@ -16,6 +21,20 @@ namespace GLExternalRAII {
 	}
 	void Window::frame_end() {
 		GLExternalUtils::glfw_frame_end(glfw_window);
+	}
+	void Window::glfw_framebuffer_size_callback(GLFWwindow* glfw_window, int width, int height)
+	{
+		Window* window = static_cast<Window*>(glfwGetWindowUserPointer(glfw_window));
+		if (window->framebuffer_size_callback) {
+			window->framebuffer_size_callback(width, height);
+		}
+	}
+	void Window::glfw_key_callback(GLFWwindow* glfw_window, int key, int scancode, int action, int mods)
+	{
+		Window* window = static_cast<Window*>(glfwGetWindowUserPointer(glfw_window));
+		if (window->key_callback) {
+			window->key_callback(key, scancode, action, mods);
+		}
 	}
 	Window::~Window() {
 		GLExternalUtils::glfw_destroy();
--- src/GLExternalRAII/glfw_window_raii.h
+++ src/GLExternalRAII/glfw_window_raii.h
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include <functional>
 #include <utility>
 
 struct GLFWwindow;
@@ -8,6 +9,8 @@ namespace GLExternalRAII {
 	class Window {
 	public:
 		GLFWwindow* glfw_window{};
+		std::function<void(int width, int height)> framebuffer_size_callback{};
+		std::function<void(int key, int scancode, int action, int mods)> key_callback{};
 
 		Window(int window_width, int window_height, int opengl_version_major, int opengl_version_minor);
 		Window(const Window&) = delete;
@@ -16,5 +19,8 @@ namespace GLExternalRAII {
 		bool is_running() const;
 		std::pair<int, int> get_width_and_height();
 		void frame_end();
+	private:
+		static void glfw_framebuffer_size_callback(GLFWwindow* glfw_window, int width, int height);
+		static void glfw_key_callback(GLFWwindow* glfw_window, int key, int scancode, int action, int mods);
 	};
 }
